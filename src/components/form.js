96c84b315c1c7429275f6fbcc5e31ebd968ee7ed
lexus2727 <lexus2424@yahoo.com>
2020-08-24 21:05:10 -0400

    setting up components

---
+++ src/components/form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form = (props) => {
+    return(
+        <form>
+            <input type="text" name="city" placeholder="Choose a City"/>
+            <input type="text" name="country" placeholder="Choose a Country" />
+            <button>Get Weather</button>
+        </form>
+
+
+    )
+}
+
+export default Form;
---
+++ src/components/heading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Heading =(prop) => {
+    return(
+        <div>
+            <h1>What's the Weather Like?</h1>
+            <h3>Find out what the weather conditions are in your city.</h3>
+        </div>
+    )
+
+    
+}
+
+export default Heading;
