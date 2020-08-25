531f2a18c3695bbbd3e61f7ae2f8014cdc19bd57
lexus2727 <lexus2424@yahoo.com>
2020-08-25 16:49:09 -0400

    api call

--- src/App.js
+++ src/App.js
@@ -3,13 +3,50 @@ import './App.css';
 import Heading from "./components/heading.js";
 import Form from "./components/form.js";
 
-function App() {
-  return (
+
+const api_key = "c947bff56ae5911bc4532e0f0849eb41";
+
+class App extends React.Component {
+
+  state = {
+    temperature: "",
+    city: "",
+    country: "",
+    humidity: "",
+    pressure: "",
+    icon: "",
+    description: "",
+    error: ""
+  }
+
+   getWeather = async (e) => {
+     const city = e.target.element.city.value;
+     const country = e.target.element.country.value;
+     e.preventDefault();
+     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&APPID=${api_key}`)
+
+     const response = await api_call.json(); //if city and state are inputted correctly then ..
+     if(city && country) {
+       this.setState({
+         temperature: response.main.temp, //main.temp was the name of the parameter in the api
+         city: response.name,
+         country: response.sys.country,
+         humidity: response.main.humidity,
+         pressure: response.main.pressure,
+         
+       })
+
+     }
+   }
+  render() {
+    return(
     <div>
-       <Heading/>
-       <Form/>
-    </div>
-  );
+      <Heading/>
+      <Form/>
+   </div>
+   )
+  }
+ 
 }
 
 export default App;
