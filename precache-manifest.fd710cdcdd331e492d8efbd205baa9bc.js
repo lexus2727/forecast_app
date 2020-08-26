81c1d7cc08023ef1824653cbf0bbe530a18b5e9d
lexus2727 <lexus2424@yahoo.com>
2020-08-26 11:51:02 -0400

    Updates

---
+++ precache-manifest.fd710cdcdd331e492d8efbd205baa9bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f83197aa83267e25ee3c2866587e6fda",
+    "url": "/forecast_app/index.html"
+  },
+  {
+    "revision": "d21d3ce965dd988cea5e",
+    "url": "/forecast_app/static/css/main.df73f6b9.chunk.css"
+  },
+  {
+    "revision": "fb42e57a9d773e80d561",
+    "url": "/forecast_app/static/js/2.5bdb35cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/forecast_app/static/js/2.5bdb35cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21d3ce965dd988cea5e",
+    "url": "/forecast_app/static/js/main.462c1dc6.chunk.js"
+  },
+  {
+    "revision": "4ccf72fb21eae5cd5abe",
+    "url": "/forecast_app/static/js/runtime-main.e96f38d3.js"
+  },
+  {
+    "revision": "d9c470c35a7db87b9e5d2fca98c5ee42",
+    "url": "/forecast_app/static/media/cloud_pic.d9c470c3.jpg"
+  }
+]);
